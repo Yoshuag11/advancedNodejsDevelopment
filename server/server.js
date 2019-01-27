282770523654af7a81f396a2255974949e451ecf
Hector Daniel Gameros <hectordgamerosm@gmail.com>
2019-01-26 17:55:09 -0800

    Add POST /todosroute and refractor mongoose

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+
+const mongoose = require( './db/mongoose' );
+const { User } = require( './models/user' );
+const { Todo } = require( './models/todo' );
+
+const app = express();
+const port = 3000;
+
+app.listen( port, () => console.log( `Listening on port ${ 3000 }` ) );
+app.use( bodyParser.json() );
+app.post( '/todos', ( req, res ) => {
+	const todo = new Todo( { text: req.body.text } );
+
+	todo.save().then(
+		doc => res.send( doc ),
+		e => res.status( 400 ).send( e ) );
+} );
