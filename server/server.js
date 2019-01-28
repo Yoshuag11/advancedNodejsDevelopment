1adf073b63b60de9024ebca88812911c0649207e
Hector Daniel Gameros <hectordgamerosm@gmail.com>
2019-01-28 10:27:55 -0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -31,14 +31,11 @@ app.get( '/todos/:id', ( req, res ) => {
 				return res.status( 404 ).send();
 			}
 
-			console.log( todo );
-
-			res.send( 'daniel' );
+			res.send( { todo } );
 		} ).
 		catch( e => {
 			res.status( 400 ).send();
 		} );
-	res.send( req.params );
 } );
 app.post( '/todos', ( req, res ) => {
 	const todo = new Todo( { text: req.body.text } );
